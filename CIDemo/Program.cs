d08266066c56e82a1df7b171eb5ce30057c432b1
dangelis <dsangelis@gmail.com>
2015-03-02 21:24:47 -0500

    Pretty much what I need.

---
+++ CIDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumClass adder = new SumClass(2, 2);
+            Console.WriteLine("The sum is {0}!", adder.TheSum);
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CIUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CIDemo;
+
+namespace CIUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSumClass()
+        {
+            int x = 2;
+            int y = 2;
+
+            var sumClassToTest = new SumClass(x, y);
+
+            Assert.AreEqual(sumClassToTest.TheSum, x + y);
+        }
+    }
+}
