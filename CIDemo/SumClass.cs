8674718ba19de874b455e4d0b2e413c6903db2f0
dangelis <dsangelis@gmail.com>
2015-03-16 22:16:46 -0400

    Adding SumClass

---
+++ CIDemo/SumClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDemo
+{
+    public class SumClass
+    {
+        public int TheSum { get; set; }
+
+        public SumClass(int x, int y)
+        {
+            // Good line
+            TheSum = x + y;
+
+            // Uh-oh, a bug!
+            //TheSum++;
+        }
+    }
+}
