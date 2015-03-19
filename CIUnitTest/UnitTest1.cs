6fcc0266d389a036f083f076766357c2d4b006bb
dangelis <dsangelis@gmail.com>
2015-03-18 23:54:00 -0400

    Add file creation on test fail

--- CIUnitTest/UnitTest1.cs
+++ CIUnitTest/UnitTest1.cs
@@ -15,6 +15,16 @@ namespace CIUnitTest
 
             var sumClassToTest = new SumClass(x, y);
 
+            if (sumClassToTest.TheSum != x + y)
+            {
+                var fileName = @"C:\Users\Public\tmpTest\CIDemoOut.txt";
+                Console.WriteLine("Unexpected sum, writing to {0}!", fileName);
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName))
+                {
+                    file.WriteLine(sumClassToTest.TheSum);
+                }
+            }
+
             Assert.AreEqual(sumClassToTest.TheSum, x + y);
         }
     }
