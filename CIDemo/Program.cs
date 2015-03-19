3c090e61c504294d48ba893c4e215d16774e5ec4
dangelis <dsangelis@gmail.com>
2015-03-18 22:57:25 -0400

    Adding file output on bad sum.

--- CIDemo/Program.cs
+++ CIDemo/Program.cs
@@ -10,8 +10,22 @@ namespace CIDemo
     {
         static void Main(string[] args)
         {
-            SumClass adder = new SumClass(2, 2);
+            var x = 2;
+            var y = 2;
+
+            SumClass adder = new SumClass(x, y);
             Console.WriteLine("The sum is {0}!", adder.TheSum);
+
+            if (adder.TheSum != x + y)
+            {
+                var fileName = @"C:\Users\Public\tmpTest\CIDemoOut.txt";
+                Console.WriteLine("Unexpected sum, writing to {0}!", fileName);
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName))
+                {
+                    file.WriteLine(adder.TheSum);
+                }
+            }
+
             Console.WriteLine("Press any key to exit!");
             Console.ReadKey();
         }
